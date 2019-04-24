d592db02767ab62b508f0728257e8e34682b8ad2
jamesbeedy <jamesbeedy@gmail.com>
2019-04-24 20:22:35 +0000

    correct vars in spark-env.sh

--- src/templates/spark-env.sh
+++ src/templates/spark-env.sh
@@ -11,7 +11,7 @@ export SPARK_WORKER_WEBUI_PORT=8081
 
 export SPARK_HOME=${SPARK_HOME:-/opt/spark}
 export SPARK_LOG_DIR=${SPARK_LOG_DIR:-/var/log/spark}
-export SPARK_LOCAL_DIRS=${SPARK_LOCAL_DIRS}:-/srv/spark_local
+export SPARK_LOCAL_DIRS=/srv/spark_local
 export SPARK_WORKER_DIR=/srv/spark_work
 
 # EXTRA SPARK + HADOOP VARS
